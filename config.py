f1f62c7970842611e943d0ea72bd2e9f9e4af380
TexSummers <86478448+TexSummers@users.noreply.github.com>
2021-11-01 02:21:41 -0300

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# tex = Person.query.filter_by(name='Tex').first()
-# tex >>> <Person 1>
-# tex.id >>> 1
-# tex.name >>> 'Tex'
-# tex.pets >>> [<Pet 1>, <Pet 3>]
-# tex.pets[0] >>> <Pet 1>
-# tex.pets[0].name >>> 'Cookie'
-# tex.pets[1].name >>> 'Spy Fish'
