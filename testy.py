d07f5587af194c5d1f4a84616ab24fb42b3c3d93
TexSummers <86478448+TexSummers@users.noreply.github.com>
2021-11-01 02:16:59 -0300

    Delete testy.py

--- testy.py
+++
@@ -1,5 +0,0 @@
-order_products = db.Table('order_products',
-                          db.Column('order_id', db.Integer, db.ForeignKey('orders.id')),
-                          db.Column('product_id', db.Integer, db.ForeignKey('products.id'))
-                          db.Column('product_qty', db.Integer)
-                          )
